89a8da7d375e96a322119ce75afeef8648a0f89e
harishumesh <harish.maddy3@gmail.com>
2022-05-15 11:48:22 +0530

    re saving the data program
    Signed-off-by: harishumesh <harish.maddy3@gmail.com>

---
+++ CI_CD_Concept/src/simple.java
@@ -0,0 +1,11 @@
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Learning the CI and CD pipeline concept for the data reposoitory ");
+	}
+
+}
